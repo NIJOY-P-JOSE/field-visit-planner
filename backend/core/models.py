5428278a2a092e70a9929d6362496e37b118cfbf
Nijoy <nijoypjosevelur@gmail.com>
2026-03-14 14:19:22 +0530

    Model setup and admin creation

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Event(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    date = models.DateField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(max_length=100)
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    leader = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Place(models.Model):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    name = models.CharField(max_length=200)
+    address = models.TextField()
+    phone = models.CharField(max_length=15)
+
+    team = models.ForeignKey(Team, on_delete=models.SET_NULL, null=True)
+
+    STATUS_CHOICES = [
+        ('pending', 'Pending'),
+        ('visited', 'Visited')
+    ]
+
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+
+    def __str__(self):
+        return self.name
