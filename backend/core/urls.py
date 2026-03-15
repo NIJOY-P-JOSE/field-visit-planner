c05c8289c21d141f4d56113fa8fd737fd0e542e8
Nijoy <nijoypjosevelur@gmail.com>
2026-03-15 10:02:24 +0530

    Urls and Views are created and Templates are fiexd

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -2,9 +2,14 @@
 from django.urls import path
 from .views import *
 
+
 urlpatterns = [
-    #path('admin/', admin.site.urls),
-    path('',home,name='home'),
-    path('places/', places, name='places'),
-    path('plan/', plan, name='plan'),
+    path("", dashboard, name="dashboard"),
+    path("login/", login_view, name="login"),
+    path("events/", events, name="events"),
+    path("places/", places, name="places"),
+    path("create-event/", create_event, name="create_event"),
+    path("create-team/", create_team, name="create_team"),
+    path("add-place/", add_place, name="add_place"),
+    path("logout/", add_place, name="logout"),
 ]
--- backend/core/views.py
+++ backend/core/views.py
@@ -2,11 +2,28 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def home(request):
-    return render(request,'core/home.html')
+def login_view(request):
+    return render(request, "core/login.html")
+
+def events(request):
+    return render(request, "core/create_event.html")
+
+
+def dashboard(request):
+    return render(request, "core/dashboard.html")
+
 
 def places(request):
-    return render(request, 'places.html')
+    return render(request, "core/places.html")
 
-def plan(request):
-    return render(request, 'plan.html')
+
+def create_event(request):
+    return render(request, "core/create_event.html")
+
+
+def create_team(request):
+    return render(request, "core/create_team.html")
+
+
+def add_place(request):
+    return render(request, "core/add_place.html")
